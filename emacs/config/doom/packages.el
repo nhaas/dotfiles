06ac034b7b5a2dfefcf437c58e2e01390a08d650
Neal Haas <nhaas@qti.qualcomm.com>
2023-01-18 17:36:56 -0800

    Add support for YAML with Doom's yaml-mode package

--- emacs/config/doom/packages.el
+++ emacs/config/doom/packages.el
@@ -16,6 +16,9 @@
 ;; Group buffers by projectile project in Ibuffer
 (package! ibuffer-projectile)
 
+;; Support for YAML mode
+(package! yaml-mode)
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
