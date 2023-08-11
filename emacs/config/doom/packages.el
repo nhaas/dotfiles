2a8e5f68bc3176874667c27b1097d8c7cef600f5
Neal Haas <nhaas@qti.qualcomm.com>
2023-08-10 21:33:52 -0700

    Remove 'ztree' package since upstream server is not secure
    'doom sync -u' aborts since codeberg.org does not have encrypted communication

--- emacs/config/doom/packages.el
+++ emacs/config/doom/packages.el
@@ -19,8 +19,8 @@
 ;; Support for YAML mode
 (package! yaml-mode)
 
-;; Diff directories recursively
-(package! ztree)
+;; ;; Diff directories recursively
+;; (package! ztree)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
